678bf90df65be1007143993fd466fa118ff8cce4
Hz <36901033+Zheng7426@users.noreply.github.com>
2020-11-15 17:54:01 -0500

    binary_search

--- Tree/script.js
+++ Tree/script.js
@@ -1,4 +1,18 @@
 const TreeNode = require('/TreeNode');
-
+const randomize = () => Math.floor(Math.random() * 20);
 const tree = new TreeNode(1);
+
+tree.addChild(15);
+const node = new TreeNode(30);
+tree.addChild(node);
 console.log(tree);
+
+tree.removeChild(15);
+console.log(tree);
+
+tree.removeChild(node);
+console.log(tree);
+
+
+tree.print();
+tree.depthFirstTraversal();
