a1184c0e6010e59e485619a184987c8a0ceab3ea
Hz <36901033+Zheng7426@users.noreply.github.com>
2020-11-18 21:48:06 -0500

    Heaps

---
+++ Heap/script.js
@@ -0,0 +1,16 @@
+const MinHeap = require('./MinHeap');
+
+const minHeap = new MinHeap();
+
+function randomize() { return Math.floor(Math.random() * 40); }
+
+for (let i=0; i < 6; i++) {
+  minHeap.add(randomize());
+}
+
+console.log('Bubbled Up', minHeap.heap);
+
+for (let i=0; i < 6; i++) {
+  minHeap.popMin();
+  console.log('Heapified', minHeap.heap);
+}
